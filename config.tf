a7a93c893aaea5d48f4e46d3326340b34703bc1f
svetlogorskiy <k.svetlogorskii@automacon.ru>
2026-01-12 02:24:39 +0300

    change config.tf

--- config.tf
+++ config.tf
@@ -71,18 +71,18 @@ resource "yandex_compute_instance" "prod" {
   }
 }
 
-output "internal_ip_address_vm_1" {
+output "internal_ip_address_build" {
   value = yandex_compute_instance.build.network_interface.0.ip_address
 }
 
-output "internal_ip_address_vm_2" {
+output "internal_ip_address_prod" {
   value = yandex_compute_instance.prod.network_interface.0.ip_address
 }
 
-output "external_ip_address_vm_1" {
+output "external_ip_address_build" {
   value = yandex_compute_instance.build.network_interface.0.nat_ip_address
 }
 
-output "external_ip_address_vm_2" {
+output "external_ip_address_prod" {
   value = yandex_compute_instance.prod.network_interface.0.nat_ip_address
 }
