8afb4488c0112037ab4ed7fbd7995f693af7eee0
svetlogorskiy <k.svetlogorskii@automacon.ru>
2026-01-12 02:48:23 +0300

    change config.tf

--- config.tf
+++ config.tf
@@ -79,12 +79,12 @@ output "internal_ip_address_build" {
   value = yandex_compute_instance.build.network_interface.0.ip_address
 }
 
-output "internal_ip_address_prod" {
-  value = yandex_compute_instance.prod.network_interface.0.ip_address
-}
-
 output "external_ip_address_build" {
   value = yandex_compute_instance.build.network_interface.0.nat_ip_address
+}
+
+output "internal_ip_address_prod" {
+  value = yandex_compute_instance.prod.network_interface.0.ip_address
 }
 
 output "external_ip_address_prod" {
